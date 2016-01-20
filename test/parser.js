45334e88aa49a1f81c354e1754aed9308a538ee9
Karl Lindmark <karl.lindmark@ninetwozero.com>
2016-01-20 01:47:35 +0100

    Huzzah, comment stripping works

--- test/parser.js
+++ test/parser.js
@@ -5,136 +5,204 @@ var multiline = require("multiline");
 var parser = require("../lib/parser");
 
 describe("Gradle build file parser", function() {
-    it("can parse a single key=>value", function() {
-        var dsl = 'key "value"';
-        var expected = { key: "value" };
-        
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
-        });
-    });
-    it("can parse a multiple key=>value combinations", function() {
-        var dsl = multiline.stripIndent(function() {/*
-            key "value"
-            key2 "value2"
-        */});
-        var expected = { key: "value", key2: 'value2' };
-        
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
-        });
-    });
-
-    it("can detect a block", function() {
-        var dsl = multiline.stripIndent(function() {/*
-            testblock {
-
-            }
-        */});
-        var expected = { testblock: {} };
-        
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
+    describe("Text parsing", function() {
+        it("can parse a single key=>value", function() {
+            var dsl = 'key "value"';
+            var expected = { key: "value" };
+            
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
         });
 
-    });
-    
-    it("can detect a single level block and its values", function() {
-        var dsl = multiline.stripIndent(function() {/*
-            testblock {
-                key1 "value1"
+        it("can parse a multiple key=>value combinations", function() {
+            var dsl = multiline.stripIndent(function() {/*
+                key "value"
                 key2 "value2"
-            }
-        */});
-        var expected = { testblock: { key1: "value1", key2: "value2" } };
+            */});
+            var expected = { key: "value", key2: 'value2' };
+            
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+        });
+
+        it("can detect a block", function() {
+            var dsl = multiline.stripIndent(function() {/*
+                testblock {
+
+                }
+            */});
+            var expected = { testblock: {} };
+            
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+
+        });
         
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
+        it("can detect a single level block and its values", function() {
+            var dsl = multiline.stripIndent(function() {/*
+                testblock {
+                    key1 "value1"
+                    key2 "value2"
+                }
+            */});
+            var expected = { testblock: { key1: "value1", key2: "value2" } };
+            
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+
         });
 
-    });
-
-    it("can detect a multiple single level block and their values", function() {
-        var dsl = multiline.stripIndent(function() {/*
-            testblock {
-                key1 "value1"
-                key2 "value2"
-            }
-            testblock2 {
-                key3 "value3"
-                key4 "value4"
-            }
-        */});
-        var expected = { testblock: { key1: "value1", key2: "value2" },
-            testblock2: { key3: "value3", key4: "value4"}
-        };
-        
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
-        });
-
-    });
-
-    it("can detect a mix of single level items", function() {
-        var dsl = multiline.stripIndent(function() {/*
-            testblock {
-                key1 "value1"
-                key2 "value2"
-            }
-            testblock3 "not really"
-            testblock2 {
-                key3 "value3"
-                key4 "value4"
-            }
-        */});
-        var expected = { testblock: { key1: "value1", key2: "value2" },
-            testblock2: { key3: "value3", key4: "value4"},
-            testblock3: "not really"
-        };
-
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
-        });
-
-    });
-
-    it("can detect chaos", function() {
-        var dsl = multiline.stripIndent(function() {/*
-            testblock {
-                key1 "value1"
-                key2 "value2"
-                nestedKey {
+        it("can detect a multiple single level block and their values", function() {
+            var dsl = multiline.stripIndent(function() {/*
+                testblock {
+                    key1 "value1"
+                    key2 "value2"
+                }
+                testblock2 {
                     key3 "value3"
                     key4 "value4"
-                    key5 {
-                        key6 "value6"
-                    }
                 }
-            }
-            testblock2 {
-                key1 "value1"
-                key2 "value2"
-            }
-            testblock3 "not really"
-        */});
-        var expected = { 
-            testblock: { 
-                key1: "value1", 
-                key2: "value2",
-                nestedKey: {
-                    key3: "value3",
-                    key4: "value4",
-                    key5: {
-                        key6: "value6"
-                    }
-                }
-            },
-            testblock2: { key1: "value1", key2: "value2"},
-            testblock3: "not really"
-        };
-        return parser.parseText(dsl).then(function(parsedValue) {
-            expect(parsedValue).to.deep.equal(expected);   
+            */});
+            var expected = { testblock: { key1: "value1", key2: "value2" },
+                testblock2: { key3: "value3", key4: "value4"}
+            };
+            
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+
         });
 
+        it("can detect a mix of single level items", function() {
+            var dsl = multiline.stripIndent(function() {/*
+                testblock {
+                    key1 "value1"
+                    key2 "value2"
+                }
+                testblock3 "not really"
+                testblock2 {
+                    key3 "value3"
+                    key4 "value4"
+                }
+            */});
+            var expected = { testblock: { key1: "value1", key2: "value2" },
+                testblock2: { key3: "value3", key4: "value4"},
+                testblock3: "not really"
+            };
+
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+
+        });
+
+        it("can detect chaos", function() {
+            var dsl = multiline.stripIndent(function() {/*
+                testblock {
+                    key1 "value1"
+                    key2 "value2"
+                    nestedKey {
+                        key3 "value3"
+                        key4 "value4"
+                        key5 {
+                            key6 "value6"
+                        }
+                    }
+                }
+                testblock2 {
+                    key1 "value1"
+                    key2 "value2"
+                }
+                testblock3 "not really"
+            */});
+            var expected = { 
+                testblock: { 
+                    key1: "value1", 
+                    key2: "value2",
+                    nestedKey: {
+                        key3: "value3",
+                        key4: "value4",
+                        key5: {
+                            key6: "value6"
+                        }
+                    }
+                },
+                testblock2: { key1: "value1", key2: "value2"},
+                testblock3: "not really"
+            };
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+
+        });
+        
+        it.only("will skip commented lines", function() {
+            var dsl = 'key "value"' + "\n";
+            dsl += '// this is a single line comment' + "\n";
+            dsl += 'key2 "value2"' + "\n";
+            dsl += '/* this is a multi' + "\n";
+            dsl += 'line comment */' + "\n";
+            dsl += '    key3 "value3"' + "\n";
+            dsl += '/**' + "\n";
+            dsl += ' *' + "\n";
+            dsl += ' * Something here' + "\n";
+            dsl += ' *' + "\n";
+            dsl += ' */';
+
+            var expected = { key: "value", key2: 'value2', key3: "value3" };
+            return parser.parseText(dsl).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+        });
+    });
+
+    describe("File parsing", function() {
+        it("should be able to parse the small sample gradle file", function() {
+            var sampleFilePath = "test/sample-data/small.build.gradle";
+            var expected = { 
+                testblock: { 
+                    key1: "value1", 
+                    key2: "value2",
+                    nestedKey: {
+                        key3: "value3",
+                        key4: "value4",
+                        key5: {
+                            key6: "value6"
+                        }
+                    }
+                },
+                testblock2: { key1: "value1", key2: "value2"},
+                testblock3: "not really"
+            };
+            return parser.parseFile(sampleFilePath).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+        });
+
+        it("should be able to parse the muzei gradle file", function() {
+            var sampleFilePath = "test/sample-data/muzei.build.gradle";
+            var expected = { 
+                testblock: { 
+                    key1: "value1", 
+                    key2: "value2",
+                    nestedKey: {
+                        key3: "value3",
+                        key4: "value4",
+                        key5: {
+                            key6: "value6"
+                        }
+                    }
+                },
+                testblock2: { key1: "value1", key2: "value2"},
+                testblock3: "not really"
+            };
+            return parser.parseFile(sampleFilePath).then(function(parsedValue) {
+                expect(parsedValue).to.deep.equal(expected);   
+            });
+        });
     });
 });
