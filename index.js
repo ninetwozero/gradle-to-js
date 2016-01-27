cf4b710e1ea08bbbaa4bbc90f1c8afba9d4f680b
Karl Lindmark <karl.lindmark@ninetwozero.com>
2016-01-27 01:57:05 +0100

    Add runnable sample (index.js)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// TODO
+#!/usr/bin/env node
+'use strict';
+
+var fs = require('fs');
+var parser = require('./lib/parser');
+var path = process.argv.slice(2).join('');
+
+if (!path) {
+  console.error('No input detected');
+  return;
+}
+
+if (fs.statSync(path)) {
+  parser.parseFile(path).then(function(parsedValue) {
+    console.log(JSON.stringify(parsedValue, '', 2));
+  });
+}
+
